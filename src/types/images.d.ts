de596a769da1dc0f2d7aa2166285bb8c48eff21e
Sirius Nottin <30043962+siriusnottin@users.noreply.github.com>
2025-06-07 09:13:12 +0200

    refactor(images): remove unused image module declarations

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -12,18 +12,3 @@ declare module '*.webp?width=600' {
   const src: string;
   export default src;
 }
-
-declare module '*.webp' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.jpg' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.png' {
-  const content: string;
-  export default content;
-} 
